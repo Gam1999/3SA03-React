66ce49a7a15967ebbccbaeabe35e7d62cf0d9cdc
Rattawan Putnual <49346370+Gam1999@users.noreply.github.com>
2019-09-22 01:53:30 +0700

    Add files via upload
    มีการเปลี่ยนคำศัพท์จาก Hello เป็น PARIS และมีการเปลี่ยนสีกล่องใส่ตัวอักษร

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render(){
       return(
         <div className="App">
-          <WordCard value="hello"/>
+          <WordCard value="PARIS"/>
         </div>
       );
     }
